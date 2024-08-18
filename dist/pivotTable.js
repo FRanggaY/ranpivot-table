f0261968db1082c2cd218155dac0112548b1100a
FRanggaY <franciscusrangga@gmail.com>
2024-08-18 16:08:48 +0700

    feat : heatmap legend

--- dist/pivotTable.js
+++ dist/pivotTable.js
@@ -10,6 +10,7 @@ class RanPivotTable {
      * @param {boolean} [heatmapOptions.enableHeatmap=false] - Enable overall heatmap.
      * @param {boolean} [heatmapOptions.enableRowHeatmap=false] - Enable row-based heatmap.
      * @param {boolean} [heatmapOptions.enableColHeatmap=false] - Enable column-based heatmap.
+     * @param {boolean} [heatmapOptions.showLegend=false] - Show the color legend for the heatmap.
      */
     constructor(data, rowFields, columnFields, valueField, aggregationFunction = 'sum', heatmapOptions = {}) {
         this.data = data;
@@ -22,7 +23,8 @@ class RanPivotTable {
         this.heatmapOptions = {
             enableHeatmap: heatmapOptions.enableHeatmap || false,
             enableRowHeatmap: heatmapOptions.enableRowHeatmap || false,
-            enableColHeatmap: heatmapOptions.enableColHeatmap || false
+            enableColHeatmap: heatmapOptions.enableColHeatmap || false,
+            showLegend: heatmapOptions.showLegend || false
         };
     }
 
@@ -32,7 +34,15 @@ class RanPivotTable {
      */
     render() {
         const { columnGroups, rowHeaders, dataMatrix } = this.processData();
-        return this.buildHtml(columnGroups, rowHeaders, dataMatrix);
+        let html = this.buildHtml(columnGroups, rowHeaders, dataMatrix);
+        
+        // If showLegend is enabled, append the legend to the HTML
+        if (this.heatmapOptions.showLegend) {
+            const { min, max } = this.getMinMaxValues(dataMatrix);
+            html += this.buildLegend(min, max);
+        }
+
+        return html;
     }
 
     processData() {
@@ -201,6 +211,33 @@ class RanPivotTable {
         return color;
     }
 
+    buildLegend(min, max) {
+        const gradientColors = [];
+        const steps = 10; // Number of gradient steps
+        const stepValue = (max - min) / steps;
+
+        for (let i = 0; i < steps; i++) {
+            const startValue = min + stepValue * i;
+            const endValue = min + stepValue * (i + 1);
+            const color = this.getHeatmapColor(startValue, min, max, null, null, null);
+            gradientColors.push({ color, range: `${startValue.toFixed(2)} - ${endValue.toFixed(2)}` });
+        }
+
+        let legendHtml = '<div style="display: flex; align-items: center; margin-top: 10px;">';
+        legendHtml += '<div style="margin-right: 10px;">Legend:</div>';
+        
+        gradientColors.forEach((entry) => {
+            legendHtml += `
+                <div style="display: flex; align-items: center; margin-right: 15px;">
+                    <div style="width: 20px; height: 20px; background-color: ${entry.color}; margin-right: 5px;"></div>
+                    <div>${entry.range}</div>
+                </div>`;
+        });
+
+        legendHtml += '</div>';
+        
+        return legendHtml;
+    }
 }
 
 // For modern JavaScript frameworks and tools that support ES modules
